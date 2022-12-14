6c65b94dc23a84a4cc0904225ace7e2062674880
Sameer Kulkarni <110413065+ksameer18@users.noreply.github.com>
2022-12-14 19:19:35 +0530

    Create uploadfile.ps1

---
+++ environments/env1/scripts/uploadfile.ps1
@@ -0,0 +1,19 @@
+param(
+[string] $ResourceGroupName,
+[string] $RawDataLakeAccountName
+)
+Write-Host "file upload script started"
+ if ((Get-Module -ListAvailable Az.Accounts) -eq $null)
+	{
+       Install-Module -Name Az.Accounts -Force
+    }
+
+$uri = "https://raw.githubusercontent.com/CSALabsAutomation/mlops-with-azure-machine-learning/main/environments/env1/Artifacts/wine-quality-data.csv";
+$bacpacFileName = "wine-quality-data.csv";
+
+$storageaccountkey = Get-AzStorageAccountKey -ResourceGroupName $Resourcegroupname -Name $RawDataLakeAccountName;
+
+$ctx = New-AzStorageContext -StorageAccountName $RawDataLakeAccountName -StorageAccountKey $storageaccountkey.Value[0]
+
+Invoke-WebRequest -Uri $uri -OutFile $bacpacFileName
+Set-AzStorageBlobContent -File $bacpacFileName -Container "raw" -Blob 'wine-quality-data.csv' -Context $ctx
