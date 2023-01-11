606b3b4fab7f9c5a4f507d6fcfda2f0635aed853
Sameer Kulkarni <110413065+ksameer18@users.noreply.github.com>
2023-01-11 21:11:59 +0530

    Update validation.ps1

--- steps/1_using-an-azure-machine-learning-job-for-automation/checks/1_used-an-azure-machine-learning-job-for-automation/validation.ps1
+++ steps/1_using-an-azure-machine-learning-job-for-automation/checks/1_used-an-azure-machine-learning-job-for-automation/validation.ps1
@@ -1,1 +1,30 @@
-ToDo
+param(
+    [string]
+    $Resourcegroupname
+)
+
+$WorkSpace = Get-AzResource -ResourceGroupName $Resourcegroupname -Resourcetype Microsoft.MachineLearningServices/workspaces
+$WorkSpacename = $WorkSpace.Name
+
+#Get azure access token
+$token = (Get-AzAccessToken).Token
+
+#Get subscription Id
+$subscriptionUri = "https://management.azure.com/subscriptions?api-version=2020-01-01"
+$headers1 = @{ Authorization = "Bearer $token"; 'ContentType' = "application/json"}
+$res1 = Invoke-RestMethod -Method Get -ContentType "application/json" -Uri $subscriptionUri -Headers $headers1
+$SubscriptionId = $res1.value[0].subscriptionId
+
+#Get jobs
+$endpointUri = "https://management.azure.com/subscriptions/"+$SubscriptionId+"/resourceGroups/"+$Resourcegroupname+"/providers/Microsoft.MachineLearningServices/workspaces/"+$WorkSpacename+"/jobs?api-version=2022-10-01"
+$headers = @{ Authorization = "Bearer $token"; 'ContentType' = "application/json"}
+$res2 = Invoke-RestMethod -Method Get -ContentType "application/json" -Uri $endpointUri -Headers $headers
+
+if($res2.value.name)
+{
+    Write-Host "Job is created"
+}
+else
+{
+   Write-Host "Job is not created" 
+}
