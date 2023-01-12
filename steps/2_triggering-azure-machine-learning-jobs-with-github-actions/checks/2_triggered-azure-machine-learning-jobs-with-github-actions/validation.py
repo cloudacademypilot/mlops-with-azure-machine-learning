1f24b5831ad7fbfb090fad960cd8efaf653715f5
Aditya Hoode <v-ahoode@microsoft.com>
2023-01-12 13:52:30 +0530

    added 02 validation.py

---
+++ steps/2_triggering-azure-machine-learning-jobs-with-github-actions/checks/2_triggered-azure-machine-learning-jobs-with-github-actions/validation.py
@@ -0,0 +1,45 @@
+import json
+import logging
+import requests
+from azure.identity import AzureCliCredential
+
+def handler():
+    try:
+        with open("steps/2_triggering-azure-machine-learning-jobs-with-github-actions/checks/2_triggered-azure-machine-learning-jobs-with-github-actions/params.json") as f:
+            params = json.load(f)
+
+        subscriptionId = params['subscription_id']
+        rgName = params['resource_group_name']
+
+        credential = AzureCliCredential()
+        token = credential.get_token("https://management.azure.com").token
+
+        headers = {
+            'ContentType': 'application/json',
+            'Authorization': f'Bearer {token}'
+        }
+
+        # Get ML Workspace name
+        listWsUrl = f'https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/{rgName}/providers/Microsoft.MachineLearningServices/workspaces?api-version=2022-10-01'
+        wsRes = requests.get(url=listWsUrl, headers=headers)
+        workspaceName = ''
+        if wsRes.status_code == 200:
+            workspaces = wsRes.json()['value']
+            if len(workspaces) != 0:
+                workspaceName = workspaces[0]['name']
+
+        # Get Jobs
+        listJobsUrl = f'https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/{rgName}/providers/Microsoft.MachineLearningServices/workspaces/{workspaceName}/jobs?api-version=2022-10-01'
+        jobsRes = requests.get(url=listJobsUrl, headers=headers)
+        if jobsRes.status_code == 200:
+            jobs = jobsRes.json()['value']
+            if len(jobs) >= 2:
+                print("Jobs created")
+            else:
+                print("Jobs not created")
+
+    except Exception as e:
+        logging.error(e)
+        
+if __name__=="__main__":
+    handler()
