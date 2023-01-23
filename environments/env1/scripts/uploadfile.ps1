3c144f6e72e691463f380906ce7884eead81d4ac
lrakai <lmrakai@gmail.com>
2023-01-22 20:57:51 -0700

    Initial infra adjustments

--- environments/env1/scripts/uploadfile.ps1
+++ environments/env1/scripts/uploadfile.ps1
@@ -8,7 +8,7 @@ Write-Host "file upload script started"
        Install-Module -Name Az.Accounts -Force
     }
 
-$uri = "https://raw.githubusercontent.com/CSALabsAutomation/mlops-with-azure-machine-learning/main/environments/env1/Artifacts/nyc-taxi-data.csv";
+$uri = "https://mscalabshare.blob.core.windows.net/assets/mlops/data/nyc-taxi-data.csv?sp=r&st=2023-01-23T03:32:39Z&se=2099-01-23T11:32:39Z&spr=https&sv=2021-06-08&sr=b&sig=exHo%2B%2FKRnv%2BuBzSJAhbtzXgKL9aY69uctNfUi%2F5H3B8%3D";
 $bacpacFileName = "nyc-taxi-data.csv";
 
 $StorageAccountKey = (Get-AzStorageAccountKey -ResourceGroupName $ResourceGroupName -Name $RawDataLakeAccountName)[0].Value;
