1edc72da6cfb00cd465cdb157adea41addf10ae8
lrakai <lmrakai@gmail.com>
2023-01-26 12:29:00 -0700

    update check 3

--- steps/5_working-with-environments-in-github-actions/checks/5_worked-with-environments-in-github-actions/source.py
+++ steps/5_working-with-environments-in-github-actions/checks/5_worked-with-environments-in-github-actions/source.py
@@ -1,45 +1,48 @@
 import json
-import logging
 import requests
-from azure.identity import AzureCliCredential
+from azure.identity import ClientSecretCredential, SharedTokenCacheCredential
+from azure.mgmt.resource import ResourceManagementClient
 
-def handler():
-    try:
-        with open("steps/5_working-with-environments-in-github-actions/checks/5_worked-with-environments-in-github-actions/params.json") as f:
-            params = json.load(f)
+def with_hint(result, hint=None):
+    return {'result': result, 'hint_message': hint} if hint else result
 
-        subscriptionId = params['subscription_id']
-        rgName = params['resource_group_name']
+def handler(event, context):
+    credentials, subscriptionId = get_credentials(event)
+    rgName = event['environment_params']['resource_group']
 
-        credential = AzureCliCredential()
-        token = credential.get_token("https://management.azure.com").token
+    bearer_token = credentials.get_token('https://management.azure.com/.default')
 
-        headers = {
-            'ContentType': 'application/json',
-            'Authorization': f'Bearer {token}'
-        }
+    headers = {
+        'ContentType': 'application/json',
+        'Authorization': f'Bearer {bearer_token.token}'
+    }
 
-        # Get ML Workspace name
-        listWsUrl = f'https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/{rgName}/providers/Microsoft.MachineLearningServices/workspaces?api-version=2022-10-01'
-        wsRes = requests.get(url=listWsUrl, headers=headers)
-        workspaceName = ''
-        if wsRes.status_code == 200:
-            workspaces = wsRes.json()['value']
-            if len(workspaces) != 0:
-                workspaceName = workspaces[0]['name']
+    # Get ML Workspace name
+    listWsUrl = f'https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/{rgName}/providers/Microsoft.MachineLearningServices/workspaces?api-version=2022-10-01'
+    wsRes = requests.get(url=listWsUrl, headers=headers)
+    workspaceName = None
+    if wsRes.status_code == 200:
+        workspaces = wsRes.json()['value']
+        if len(workspaces) != 0:
+            workspaceName = workspaces[0]['name']
+    if not workspaceName:
+        return with_hint(False, 'Azure Machine Learning workspace not found')
 
-        # Get Jobs
-        listJobsUrl = f'https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/{rgName}/providers/Microsoft.MachineLearningServices/workspaces/{workspaceName}/jobs?api-version=2022-10-01'
-        jobsRes = requests.get(url=listJobsUrl, headers=headers)
-        if jobsRes.status_code == 200:
-            jobs = jobsRes.json()['value']
-            if len(jobs) >= 4:
-                print("Jobs created")
-            else:
-                print("Jobs not created")
+    # Get Jobs
+    listJobsUrl = f'https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/{rgName}/providers/Microsoft.MachineLearningServices/workspaces/{workspaceName}/jobs?api-version=2022-10-01'
+    jobsRes = requests.get(url=listJobsUrl, headers=headers)
+    jobs = []
+    if jobsRes.status_code == 200:
+        jobs = jobsRes.json()['value']
 
-    except Exception as e:
-        logging.error(e)
-        
-if __name__=="__main__":
-    handler()
+    return with_hint(len(jobs) >= 4, f'Expected to find (at least) 4 Azure Machine Learning jobs but found {len(jobs)} instead. Ensure all machine learning jobs have ran.')
+
+
+def get_credentials(event):
+    subscription_id = event['environment_params']['subscription_id']
+    credentials = ClientSecretCredential(
+        client_id=event['credentials']['credential_id'],
+        client_secret=event['credentials']['credential_key'],
+        tenant_id=event['environment_params']['tenant']
+    )
+    return credentials, subscription_id
